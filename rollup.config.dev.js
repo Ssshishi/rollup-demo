40803b200c5a388d1b4d2211363efc5a535c46f7
shenshi <shenshi@shouqianba.com>
2021-08-27 18:34:31 +0800

    feat: 开启本地服务器

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -4,6 +4,7 @@ import babel from 'rollup-plugin-babel'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import postcss from 'rollup-plugin-postcss'
+import serve from 'rollup-plugin-serve'
 
 export default {
   input: 'src/main.js',
@@ -39,6 +40,11 @@ export default {
     terser(), //转译es6+语法 的 解析器，mangler和压缩器工具包       uglify-js只能翻译es5语法
     babel({
       exclude: 'node_modules/*',
+    }),
+    serve({
+      open: true,
+      port: 3000,
+      contentBase: '',
     }),
   ],
   external: ['loadsh'], //配置external 不打包 loadsh 第三方包内容， 但是需要外部引入loadsh
